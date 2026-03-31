4cae92a6022f1e4fa5444c81e064edb13ba81da7
Vicky's Net <chimevictor365@gmail.com>
2026-03-31 13:27:49 +0100

    Update contact.py

--- contact.py
+++ contact.py
@@ -44,4 +44,5 @@ def contact():
     return redirect(url_for('index'))
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    # host='0.0.0.0' is the key to mobile access
+    app.run(debug=True, host='0.0.0.0', port=5000)
